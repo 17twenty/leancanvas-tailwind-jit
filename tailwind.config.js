d19ca796aad15c04558af33421451368e42f0f57
Nick Glynn <n.s.glynn@gmail.com>
2021-03-20 19:56:52 +1100

    Push usecase

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,ts}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
